ae3feb3d6b657568023a0d9cc42dd44812bcb348
spindleshanks <smithd216@protonmail.com>
2023-03-06 13:43:10 +0000

    Added example divs for hsitory

--- las/src/components/History.jsx
+++ las/src/components/History.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default function History() {
-    return (
-        <div>
-            <p>This is the history page</p>
-        </div>
-    );
+    let elements = [
+        "picture1",
+        "picture2",
+        "picture3",
+        "picture4",
+        "picture5",
+        "picture6",
+        "picture7",
+        "picture8",
+        "picture9",
+    ];
+    const display = elements.map((element, index) => {
+        return (
+            <div className="history-picture" key={index}>
+                {element}
+            </div>
+        );
+    });
+
+    return <div id="history-container">{display}</div>;
 }
