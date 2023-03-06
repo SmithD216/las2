df1fbfdbecd75c3d9ed8cb31e5db189a75bc2697
spindleshanks <smithd216@protonmail.com>
2023-03-06 20:38:25 +0000

    Changed dummy array to array of objects

--- las/src/components/History.jsx
+++ las/src/components/History.jsx
@@ -3,25 +3,36 @@ import "../styles/History.css";
 
 export default function History() {
     let elements = [
-        "picture 1",
-        "picture 2",
-        "picture 3",
-        "picture 4",
-        "picture 5",
-        "picture 6",
-        "picture 7",
-        "picture 8",
-        "picture 9",
+        {
+            picture: "Picture 1",
+            url: "https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png",
+        },
+        {
+            picture: "Picture 2",
+            url: "https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png",
+        },
+        {
+            picture: "Picture 3",
+            url: "https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png",
+        },
+        {
+            picture: "Picture 4",
+            url: "https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png",
+        },
+        {
+            picture: "Picture 5",
+            url: "https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png",
+        },
     ];
     const display = elements.map((element, index) => {
         return (
             <div className="history-thumbnail" key={index}>
                 <img
                     className="history-source"
-                    src="https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png"
-                    alt={element}
+                    src={element.url}
+                    alt={element.picture}
                 />
-                <p className="history-title">{element}</p>
+                <p className="history-title">{element.picture}</p>
             </div>
         );
     });
