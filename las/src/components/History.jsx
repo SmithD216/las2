038f63476332401c58bcf6efb5db2460ca067b96
spindleshanks <smithd216@protonmail.com>
2023-03-06 17:31:38 +0000

    Styled history page

--- las/src/components/History.jsx
+++ las/src/components/History.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import "../styles/History.css";
 
 export default function History() {
     let elements = [
-        "picture1",
-        "picture2",
-        "picture3",
-        "picture4",
-        "picture5",
-        "picture6",
-        "picture7",
-        "picture8",
-        "picture9",
+        "picture 1",
+        "picture 2",
+        "picture 3",
+        "picture 4",
+        "picture 5",
+        "picture 6",
+        "picture 7",
+        "picture 8",
+        "picture 9",
     ];
     const display = elements.map((element, index) => {
         return (
-            <div className="history-picture" key={index}>
-                {element}
+            <div className="history-thumbnail" key={index}>
+                <img
+                    className="history-source"
+                    src="https://i.kym-cdn.com/photos/images/newsfeed/000/218/968/2bf.png"
+                    alt={element}
+                />
+                <p className="history-title">{element}</p>
             </div>
         );
     });
