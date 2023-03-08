7552f333eb0a2634f2b5d81b768fcff8511f9dd8
spindleshanks <smithd216@protonmail.com>
2023-03-08 14:50:18 +0000

    Added fetch to history page

--- las/src/components/History.jsx
+++ las/src/components/History.jsx
@@ -1,45 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/History.css";
 
 export default function History() {
-    let elements = [
-        {
-            picture: "Picture 1",
-            url: "https://www.shutterstock.com/image-illustration/nine-square-coloured-geometric-grid-600w-1861648129.jpg",
-        },
-        {
-            picture: "Picture 2",
-            url: "https://www.shutterstock.com/image-illustration/nine-square-coloured-geometric-grid-600w-1861648129.jpg",
-        },
-        {
-            picture: "Picture 3",
-            url: "https://www.shutterstock.com/image-illustration/nine-square-coloured-geometric-grid-600w-1861648129.jpg",
-        },
-        {
-            picture: "Picture 4",
-            url: "https://www.shutterstock.com/image-illustration/nine-square-coloured-geometric-grid-600w-1861648129.jpg",
-        },
-        {
-            picture: "Picture 5",
-            url: "https://www.shutterstock.com/image-illustration/nine-square-coloured-geometric-grid-600w-1861648129.jpg",
-        },
-    ];
-    const display = elements.map((element, index) => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/photos`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setPosts(data);
+            });
+    }, []);
+
+    const display = posts.slice(0, 10).map((element, index) => {
         return (
-            <div className="history-thumbnail" key={index}>
+            <div className="history-thumbnail" key={element.id}>
                 <Link to={`/history/${index + 1}`}>
                     <img
                         className="history-source"
-                        src={element.url}
-                        alt={element.picture}
+                        alt="picture"
+                        src={element.thumbnailUrl}
                     />
                 </Link>
 
-                <p className="history-title">{element.picture}</p>
+                <p className="history-title">{element.title}</p>
             </div>
         );
     });
 
-    return <div id="history-container">{display}</div>;
+    return (
+        <>
+            {display ? (
+                <div id="history-container">{display}</div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </>
+    );
 }
