797d34a867b99735333f58a5983a0e7665b54576
spindleshanks <smithd216@protonmail.com>
2023-03-08 12:44:20 +0000

    Added controlled component to submit page

--- las/src/components/Submit.jsx
+++ las/src/components/Submit.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/Submit.css";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Container } from "react-bootstrap";
 
 export default function Submit() {
+    const [urlValue, setUrlValue] = useState("");
+    const onChange = (e) => {
+        setUrlValue(e.target.value);
+    };
     return (
         <div id="submission-form" className="mt-5 mx-auto">
             <Form className="submissions">
@@ -17,8 +23,20 @@ export default function Submit() {
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formUrl">
                     <Form.Label>URL</Form.Label>
-                    <Form.Control type="url" placeholder="https://imgur.com/" />
+                    <Form.Control
+                        type="url"
+                        placeholder="https://imgur.com/"
+                        onChange={onChange}
+                    />
                 </Form.Group>
+                <Container>
+                    <img
+                        id="submit-preview"
+                        className="img-fluid rounded d-block mx-auto mb-2"
+                        src={urlValue}
+                        alt=""
+                    />
+                </Container>
                 <Button
                     variant="primary"
                     type="submit"
