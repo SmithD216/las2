6dbde4bfb794279dac13d65be557efaab4dd9220
spindleshanks <smithd216@protonmail.com>
2023-03-10 10:46:14 +0000

    FInished formatting and styling of historydetail

--- las/src/components/HistoryDetails.jsx
+++ las/src/components/HistoryDetails.jsx
@@ -17,12 +17,23 @@ export default function HistoryDetails() {
     }, []);
 
     return detail[picture] ? (
-        <div id="history-detail-card">
-            <img
-                className="history-detail-image d-block mx-auto mt-3 mb-3"
-                alt="picture"
-                src={detail[picture].url}
-            />
+        <div id="history-detail-card" className="card mx-auto mt-3 mb-3">
+            <a href={detail[picture].url}>
+                <img
+                    className="history-detail-image card-img-top"
+                    alt="picture"
+                    src={detail[picture].url}
+                />
+            </a>
+            <div className="card-body">
+                <h4 className="card-title">{detail[picture].title}</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ullam assumenda totam fugiat alias mollitia ea, eveniet
+                    magni eligendi delectus sit itaque id. Eius, id vel.
+                    Quisquam necessitatibus ipsam ducimus facere?
+                </p>
+            </div>
         </div>
     ) : (
         <div>Loading...</div>
