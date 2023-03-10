6ccf73efacb3464069fbd753316d1d5698e4a2cc
spindleshanks <smithd216@protonmail.com>
2023-03-10 09:25:26 +0000

    Showing full picture in historydetails

--- las/src/components/HistoryDetails.jsx
+++ las/src/components/HistoryDetails.jsx
@@ -5,17 +5,24 @@ export default function HistoryDetails() {
     const { picture } = useParams();
     const [detail, setDetail] = useState([1]);
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/photos`)
+        fetch(`https://jsonplaceholder.typicode.com/photos?${picture}`)
             .then((res) => {
                 return res.json();
             })
             .then((data) => {
                 setDetail(data);
+                console.log(detail);
             });
     }, []);
 
     return detail[picture] ? (
-        <div>{detail[picture].title}</div>
+        <div>
+            <img
+                className="history-detail-image"
+                alt="picture"
+                src={detail[picture].url}
+            />
+        </div>
     ) : (
         <div>Loading...</div>
     );
